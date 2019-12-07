c724c09317d95c24931afe44cad70b6819ca324d
tomt <tomt@student.chalmers.se>
2019-12-07 15:12:01 +0100

    Day2 complete

--- Day2/Second/Puzzle2.hs
+++ Day2/Second/Puzzle2.hs
@@ -1,35 +1,51 @@
 import Functions
 import System.IO
 import Data.List.Split
+import qualified Data.List.Safe as Safe
 import Data.Char
 import Text.Read
+import Debug.Trace
 main :: IO ()
 main = openFile "puzzle1.txt" ReadMode >>= 
        hGetContents >>= \numbers ->
-       print $ head $ g [(u,v) | u <- [0..50], v <- [0..50]] (splitOn "," numbers)
+       print $ g [(u,v) | u <- [0..99], v <- [0..99]] (splitOn "," numbers)
 
 doOperations :: [String] -> Int -> [String]-> [String]
 doOperations [] _ nyaList= nyaList
-doOperations list n nyaList= do
-    let pos1 = f(list !! (n+1))
-    let pos2 = f(list !! (n+2))
-    let var  = f(list !! (n+3))
-    if (n >= ((length list)-4)) then 
-        doOperations [] n nyaList 
-    
-    else if (list !! n) == "1" then let newList = setAt (f(list !! var)) (show((f(list !! pos1))+(f(list !! pos2)))) list in (doOperations newList (n+4) newList)
+doOperations list@(x:xs) n nyaList= do
+    if (n>= (length list -3)) then doOperations [] n nyaList 
+    else do
+        s <- (Safe.!!) list (n+1)
+        let pos1 = f s
+        st <- (Safe.!!) list (n+2)
+        let pos2 = f st
+        str <- (Safe.!!) list (n+3)
+        let var = f str
+        num <- (Safe.!!) list (n)
 
-            else if (list !! n) == "2" then let newList = setAt (f(list !! var)) (show((f(list !! pos1))*(f(list !! pos2)))) list in (doOperations newList (n+4) newList)
+        p1 <- (Safe.!!) list pos1
+        let posM1 = f p1
+        p2 <- (Safe.!!) list pos2
+        let posM2 = f p2
+        v1 <- (Safe.!!) list var
+        let posMVar = f v1
+        
+        if (n >= ((length list)-4)) then 
+            doOperations [] n nyaList 
+            
+        else if num == "1" then let newList = setAt posMVar (show(posM1+posM2)) list in (doOperations newList (n+4) newList)
 
-                 else if (list !! n) == "99" then doOperations [] 0 nyaList
-                      else doOperations [] 0 ["fel"] 
+               else if num == "2" then let newList = setAt posMVar (show(posM1*posM2)) list in (doOperations newList (n+4) newList)
+
+                     else if num == "99" then doOperations [] 0 nyaList
+                          else doOperations [] 0 ["fel"] 
                
             
 f :: String -> Int
 f s = read s :: Int
 
 g :: [(Int, Int)] -> [String] -> [String]
-g [] list = ["Nothing","Nothing"]
+g [] list = []
 g ((u, v):xs) list = do
     let l1 = setAt 1 (show u) list
     let l2 = setAt 2 (show v) l1
@@ -37,4 +53,4 @@ g ((u, v):xs) list = do
     if g' l2 == Just 19690720 then (show u):" ":(show v):[] else g xs list
 
 g' :: [String] -> Maybe Int
-g' s = readMaybe ( head (tail ( doOperations s 0 [] )))
+g' s = Safe.tail ( doOperations s 0 [] ) >>= Safe.head >>= readMaybe
