98e5d5f4059e9b997f462142113ed584ecb25e76
tomt <tomt@student.chalmers.se>
2019-12-09 23:40:22 +0100

    update

--- Day4/Second/Puzzle2.hs
+++ Day4/Second/Puzzle2.hs
@@ -1,7 +1,7 @@
 import Data.List
 import Debug.Trace
 main :: IO ()
-main = print . length $ [x | x <- [387638..919123], (f $ show x) && (g $ show x) && (h $ show x)]
+main = print . length $ [x | x <- [387638..919123], f (show x) && g (show x) && h (show x)]
 
 f :: String -> Bool
 f [] = False
@@ -14,5 +14,7 @@ g [x] = True
 g (x:y:xs) = x == y || g (y:xs)
 
 h :: String -> Bool
-h s = ((elem 3 . map length . group $ s) && (elem 2 . map length . group $ s)) || (elem 2 . map length . group $ s)
+h s = ((elem 3 . map length . group $ s) && 
+       (elem 2 . map length . group $ s)) || 
+       (elem 2 . map length . group $ s)
 
