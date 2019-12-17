689a224f230fbc6d6a3f267a62172f6209ae3abc
tomt <tomt@student.chalmers.se>
2019-12-17 09:54:04 +0100

    upgrades

--- Day6/First/Puzzle1.hs
+++ Day6/First/Puzzle1.hs
@@ -4,17 +4,17 @@ import Data.List.Split
 import Data.List
 import qualified Data.Map as Map
 
-type Abel = Map.Map String String
+type Abel = Map.Map String Int
 
 main :: IO ()
 main = openFile "puzzle1.txt" ReadMode >>= hGetContents >>= \input ->
-       print $ foldl (\a x -> a + ((fromIntegral $ length $ x) / 3) ) 0 $ orbits ( map (splitOn ")") $ lines $ input ) Map.empty
+       print $ foldl (\a x -> a + ((fromIntegral $ x) / 3) ) 0 $ orbits ( map (splitOn ")") $ lines $ input ) Map.empty
 
 orbits :: [[String]] -> Abel -> Abel
 orbits [] abel = abel
 orbits ((x:y:xs):xss) abel = do 
-    let val = orbits' x y abel
+    let val = orbits' x 1 abel
     orbits xss val
 
-orbits' :: String -> String -> Abel -> Abel
-orbits' x y z = Map.insertWith (++) x y z
+orbits' :: String -> Int -> Abel -> Abel
+orbits' = Map.insertWith (+)
